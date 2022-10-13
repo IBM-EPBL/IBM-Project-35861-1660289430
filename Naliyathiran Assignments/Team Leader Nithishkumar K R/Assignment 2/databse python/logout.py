eb20ad0297265b570ae81254c5afa6dafd4f6a3c
Nithishkumar-cloud <krnithishkumar14@gmail.com>
2022-10-13 20:13:33 +0530

    ass2

---
+++ Naliyathiran Assignments/Team Leader Nithishkumar K R/Assignment 2/databse python/logout.py
@@ -0,0 +1,9 @@
+# this will be the logout page
+@app.route('/pythonlogin/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
--- Naliyathiran Assignments/Team Leader Nithishkumar K R/README.md
+++
@@ -1,2 +0,0 @@
-# IBM-Project-35861-1660289430
-News Tracker Application
